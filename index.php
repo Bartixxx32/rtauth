708258310d11282df5b19458c65b8d62a6d71fc1
Bartixxx32 <mcbplay1@gmail.com>
2020-12-27 01:51:28 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
+<?php
+$password = $_POST["password"]; # in our current example, this will be 'supersecret'
 
+if ($password == "23042001") {
+  http_response_code(201); # return 201 "Created"
+} else {
+  http_response_code(404); # return 404 "Not Found"
+}
+?>
