b4b4a2234eb9d86e973f7ce2940dba127c59cb0d
Bartixxx32 <mcbplay1@gmail.com>
2020-12-27 01:48:34 +0100

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,12 +1,7 @@
 <?php
-$username = $_POST["name"]; # in our current example, this will be 'john'
-$password = $_POST["psk"]; # in our current example, this will be 'supersecret'
+$password = $_POST["password"]; # in our current example, this will be 'supersecret'
 
-$valid_users = array("john" => "supersecret",
-                     "winnie" => "thepooh",
-					 "batman" => "nananananananana");
-
-if ($valid_users[$username] == $password) {
+if ($password == "23042001") {
   http_response_code(201); # return 201 "Created"
 } else {
   http_response_code(404); # return 404 "Not Found"
